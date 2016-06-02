be4e1882b6a10941089dfa97869613dd990ae700
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-06-02 16:18:05 +0200

    [feature] implement socket.io connection and server

--- app/assets/scripts/js/app.js
+++ app/assets/scripts/js/app.js
@@ -1,1 +1,8 @@
-import $ from 'jquery';
+var socket = io();
+
+$(document).ready(function() {
+	$('.form-subscribe').on('click', '.btn-primary', function() {
+		var searchValue = $('#searchValue').val();
+		socket.emit('searchValue', searchValue)
+	})
+})
