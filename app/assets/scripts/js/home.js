6f29af5428614d4ff80364a44f8b5b7c9c47e771
Nicola Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2016-06-02 16:30:37 +0200

    [bugfix] remove testcode from home.js

--- app/assets/scripts/js/home.js
+++ app/assets/scripts/js/home.js
@@ -3,29 +3,6 @@
  */
 import $ from 'jquery';
 
-var solr = require('solr-client');
-
-var options = {
-	host : 'localhost', //IP or localhost - should be loaded from config file so that it does not appear in Git
-	port : '8983',
-	core: 'movies'
-}
-
-var client = solr.createClient(options);
-
-// Lucene query
-var query2 = client.createQuery()
-	.q({title : 'e.g. Hannibal'})
-	.start(0)
-	.rows(1000);
-client.search(query2,function(err,obj){
-	if(err){
-		console.log(err);
-	}else{
-		console.log(JSON.stringify(obj));
-	}
-});
-
 function buildHTMLforQuery(results){
 	var html = $('');
 	var blocks = [];
